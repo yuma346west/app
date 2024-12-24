6af3a03b41f527d3d3ccfc8d54b0054eaa83eb16
yuma346west <ub.yuma.cloe@gmail.com>
2024-12-24 22:21:36 +0900

    coding layout

--- ruby/config/routes.rb
+++ ruby/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # root "application#hello"
-  root "static_pages#home"
 end
