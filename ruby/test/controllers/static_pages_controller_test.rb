daf862177d8782d43b54ef06f9ad7a3d3d6eb3dc
yuma346west <ub.yuma.cloe@gmail.com>
2024-12-25 23:56:22 +0900

    test succes

--- ruby/test/controllers/static_pages_controller_test.rb
+++ ruby/test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
   test 'should get home' do
-    get root_url
+    get root_path
     assert_response :success
     assert_select 'title', "#{@base_title}"
   end
